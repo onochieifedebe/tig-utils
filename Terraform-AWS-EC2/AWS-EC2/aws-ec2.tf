f3fd6e9f7b18432309ea97ff0ca7f949ee2d5248
onochie.ifedebe <ifedebe.o@gmail.com>
2023-04-19 10:12:39 -0500

    configured instance for provisioning

--- Terraform-AWS-EC2/AWS-EC2/aws-ec2.tf
+++ Terraform-AWS-EC2/AWS-EC2/aws-ec2.tf
@@ -13,20 +13,20 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "example_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
+# resource "aws_vpc" "example_vpc" {
+#   cidr_block = "172.31.0.0/16"
+# }
 
 # Create a subnet in the VPC
-resource "aws_subnet" "example_subnet" {
-  vpc_id     = aws_vpc.example_vpc.id
-  cidr_block = "10.0.1.0/24"
-}
+# resource "aws_subnet" "example_subnet" {
+#   vpc_id     = aws_vpc.example_vpc.id
+#   cidr_block = "172.31.32.0/20"
+# }
 
 # Create a security group for the instance
-resource "aws_security_group" "example_security_group" {
-  name_prefix = "example_security_group"
-  vpc_id      = aws_vpc.example_vpc.id
+resource "aws_security_group" "my_test_sg" {
+  name_prefix = "devops_sg"
+  vpc_id      = "vpc-040f30e4eca6d7402"
 
   ingress {
     from_port   = 22
@@ -41,23 +41,28 @@ resource "aws_security_group" "example_security_group" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    mangedBy = "terraform"
+    environment = "dev"
+  }
 }
 
 # Create a key pair for the instance
-resource "aws_key_pair" "example_key_pair" {
-  key_name   = "example_key_pair"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCnCvq7zH9M1e+vc96fE... example_key_pair"
-}
+# resource "aws_key_pair" "example_key_pair" {
+#   key_name   = "devops-key-pair"
+#   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCnCvq7zH9M1e+vc96fE... example_key_pair"
+# }
 
 # Create an EC2 instance
-resource "aws_instance" "example_instance" {
-  ami           = "ami-0c94855ba95c71c99"
+resource "aws_instance" "my_ichie_instance" {
+  ami           = "ami-069aabeee6f53e7bfv"
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.example_key_pair.key_name
-  vpc_security_group_ids = [aws_security_group.example_security_group.id]
-  subnet_id     = aws_subnet.example_subnet.id
+  key_name      = "devops-key-pair"
+  vpc_security_group_ids = [aws_security_group.my_test_sg.id]
+  subnet_id     = "subnet-02193bafd5aca89e0"
 
   tags = {
-    Name = "example_instance"
+    Name = "terraform_test_instance"
   }
 }
